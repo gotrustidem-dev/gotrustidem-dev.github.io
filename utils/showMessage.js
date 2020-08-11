99617ca4d8b39d63816f337b6f0e87c0fcf2805f
GuanHanChen <guanhan.chen@gotrustid.com>
2020-08-11 16:02:59 +0800

    Add TestPKIoverFIDOv2.html to show error message,

---
+++ utils/showMessage.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const CTAP1_ERR_SUCCESS 				= 0;
+const CTAP1_ERR_INVALID_COMMAND 		= 1;
+const CTAP2_ERR_CBOR_PARSING            = 0x10;    //Error while parsing CBOR.
+const CTAP2_ERR_CBOR_UNEXPECTED_TYPE    = 0x11;    //Invalid/unexpected CBOR error.
+const CTAP2_ERR_INVALID_CBOR            = 0x12;    //Error when parsing CBOR.
+const CTAP2_ERR_INVALID_CBOR_TYPE       = 0x13;    //Invalid or unexpected CBOR type.
+const CTAP2_ERR_MISSING_PARAMETER       = 0x14;    //Missing non-optional parameter.
+const CTAP2_ERR_LIMIT_EXCEEDED          = 0x15;    //Limit for number of items exceeded.
+const CTAP2_ERR_UNSUPPORTED_EXTENSION   = 0x16;    //Unsupported extension.
+const CTAP2_ERR_TOO_MANY_ELEMENTS       = 0x17;        //Limit for number of items exceeded.
+const CTAP2_ERR_EXTENSION_NOT_SUPPORTED = 0x18;                //Unsupported extension.
+const CTAP2_ERR_CREDENTIAL_EXCLUDED     = 0x19;          //Valid credential found in the exludeList.
+const CTAP2_ERR_CREDENTIAL_NOT_VALID    = 0x20;            //Credential not valid for authenticator.
+const CTAP2_ERR_PROCESSING              = 0x21;  //Processing (Lengthy operation is in progress).
+const CTAP2_ERR_INVALID_CREDENTIAL      = 0x22;          //Credential not valid for the authenticator.
+const CTAP2_ERR_USER_ACTION_PENDING     = 0x23;          //Authentication is waiting for user interaction.
+const CTAP2_ERR_OPERATION_PENDING       = 0x24;        //Processing, lengthy operation is in progress.
+const CTAP2_ERR_NO_OPERATIONS           = 0x25;    //No request is pending.
+const CTAP2_ERR_UNSUPPORTED_ALGORITHM   = 0x26;            //Authenticator does not support requested algorithm.
+const CTAP2_ERR_OPERATION_DENIED        = 0x27;        //Not authorized for requested operation.
+const CTAP2_ERR_KEY_STORE_FULL          = 0x28;      //Internal key storage is full.
+const CTAP2_ERR_NOT_BUSY                = 0x29;//Authenticator cannot cancel as it is not busy.
+const CTAP2_ERR_NO_OPERATION_PENDING    = 0x2A;            //No outstanding operations.
+const CTAP2_ERR_UNSUPPORTED_OPTION      = 0x2B;          //Unsupported option.
+const CTAP2_ERR_INVALID_OPTION          = 0x2C;      //Unsupported option.
+const CTAP2_ERR_KEEPALIVE_CANCEL        = 0x2D;        //Pending keep alive was cancelled.
+const CTAP2_ERR_NO_CREDENTIALS          = 0x2E;      //No valid credentials provided.
+const CTAP2_ERR_USER_ACTION_TIMEOUT     = 0x2F;          //Timeout waiting for user interaction.
+const CTAP2_ERR_NOT_ALLOWED             = 0x30;  //Continuation command, such as, authenticatorGetNextAssertion notallowed.
+const CTAP2_ERR_PIN_INVALID             = 0x31;  //PIN Invaild
+const CTAP2_ERR_PIN_BLOCKED             = 0x32;  //PIN Blocked.
+const CTAP2_ERR_PIN_AUTH_INVALID        = 0x33;        //PIN authentication,pinAuth, verification failed.
+const CTAP2_ERR_PIN_AUTH_BLOCKED        = 0x34;        //PIN authentication,pinAuth, blocked. Requires power recycle to reset.
+const CTAP2_ERR_PIN_NOT_SET             = 0x35;  //No PIN has been set.
+const CTAP2_ERR_PIN_REQUIRED            = 0x36;        //PIN is required for the selected operation.
+const CTAP2_ERR_PIN_POLICY_VIOLATION    = 0x37;            //PIN policy violation. Currently only enforces minimum length.
+const CTAP2_ERR_PIN_TOKEN_EXPIRED       = 0x38;        //pinToken expired on authenticator.
+const CTAP2_ERR_REQUEST_TOO_LARGE       = 0x39;        //Authenticator cannot handle this request due to memory constraints.
+const CTAP1_ERR_OTHER                   = 0x7F;  //Other unspecified error.
+const CTAP2_ERR_SPEC_LAST               = 0xDF;  //CTAP 2 spec last error.
+const CTAP2_ERR_EXTENSION_FIRST         = 0xE0;      //Extension specific error.
+const CTAP2_ERR_EXTENSION_LAST          = 0xEF;      //Extension specific error.
+const CTAP2_ERR_VENDOR_FIRST            = 0xF0;    //Vendor specific error.
+const CTAP2_ERR_VENDOR_LAST             = 0xFF;//Vendor specific error.
+
+
+const ErrorMsg_PIN_INVALID   = "PIN invalid.";
+const ErrorMsg_PIN_BLOCKED   = "PIN blocked.";
+
+const Msg_PIN_Trial_Counter  = "The trail counter is";
+
+// Command Header GoTrust-Idem-PKI
+var GTheaderStr = "GoTrust-Idem-PKI"; 
+
+
+
+
+
+
+
+var showCertificMessage = (buffer) => {
+
+}
+
+var showSignMessage = (buffer) => {
+
+    let gtHeader      = buffer.slice(0, 16);            buffer = buffer.slice(16);
+
+    if(String.fromCharCode.apply(null, new Uint8Array(gtHeader))===GTheaderStr){ //This is error handle
+
+        alert("Has error");
+    }else{ // show normal message
+
+        console.log("show normal message!!");
+    }
+    
+}
