212bd25f171a42a0c20c0c3a0bb1f99737d5c07d
GuanHanChen <guanhan.chen@gotrustid.com>
2020-08-21 16:54:55 +0800

    update

--- utils/showMessage.js
+++ utils/showMessage.js
@@ -190,7 +190,7 @@ var showRSAKeyPair = (buffer) => {
             case CTAP1_ERR_SUCCESS:
                 //var keydata = 
                 
-                let data = CBOR.decode(new Uint8Array(buffer));
+                let data = CBOR.decode(buffer.buffer);
                console.log('data: ', data);
                 Msg = "Command error!";
 
