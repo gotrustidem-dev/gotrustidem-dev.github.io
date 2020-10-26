255fcf6655bb6a54ebc6464dac9562f1aed40cbf
GuanHanChen <guanhan.chen@gotrustid.com>
2020-10-26 15:38:04 +0800

    update

--- utils/PKIoverFIDO.js
+++ utils/PKIoverFIDO.js
@@ -4,4 +4,41 @@
 
 
 
+/**
+ *  Return  a list of certificate that stored on token
+ * 
+ * 
+ */
+function readSimpleCerts(){
+
+
+}
+
+/** 
+ * Return a full certificate by searching the index
+ * 
+ * 
+ */
+function readCert(){
+
+
+    
+
+}
+
+
+/**
+ * 
+ *  Return the value of Signature of input data 
+ *  @returns
+ *      The value of Signature 
+ */
+
+
+function sign(){
+
+
+}
+
+
 
