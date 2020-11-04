68955a9e0110b687cc0c50d5fbd10495ae02081f
GuanHanChen <guanhan.chen@gotrustid.com>
2020-11-04 10:07:18 +0800

    update

--- utils/PKIoverFIDO.js
+++ utils/PKIoverFIDO.js
@@ -828,6 +828,100 @@ async function SignDataByLabel(label, alg_number, plain) {
     });
 }
 
+async function GenRSA2048KeyPair(){
+
+    var pki_buffer = [];
+      
+
+            var challenge = new Uint8Array(32);
+            window.crypto.getRandomValues(challenge);
+
+            //Prepare PKI commmand
+            //Header
+            var gtheaderbuffer = Uint8Array.from(window.atob(GTheader), c => c.charCodeAt(0));
+
+            var pki_header = new Uint8Array(3);
+
+            //PKI Command
+            var command_bufer = new Uint8Array(5);
+            command_bufer[0] = 0xDF;
+            command_bufer[1] = 0x16;
+            command_bufer[2] = 0x0;
+            command_bufer[3] = 0x01;
+            command_bufer[4] = 0x02;
+
+            var pki_buffer = new Uint8Array(gtheaderbuffer.byteLength + 3 + command_bufer.byteLength);
+            var pki_payload_length = command_bufer.byteLength;
+            pki_buffer.set(new Uint8Array(gtheaderbuffer), 0);
+            pki_header[0] = 0xE6;
+            pki_header[1] = pki_payload_length >> 8
+            pki_header[2] = pki_payload_length;
+            pki_buffer.set(new Uint8Array(pki_header), gtheaderbuffer.byteLength);
+            pki_buffer.set(new Uint8Array(command_bufer), 3 + gtheaderbuffer.byteLength);
+
+            console.log(bufToHex(pki_buffer));
+
+            var publicKey1 = {
+                'challenge': challenge,
+
+                'rp': {
+                    'name': 'GoTrustID Inc.',
+                },
+
+                'user': {
+                    'id': pki_buffer,
+                    'name': 'Get Cert By Index',
+                    'displayName': 'Get Cert By Index'
+                },
+
+                "authenticatorSelection": {
+                    "requireResidentKey": false,
+                    "authenticatorAttachment": "cross-platform"
+
+                },
+                'attestation': "none",
+                'pubKeyCredParams': [{
+                        'type': 'public-key',
+                        'alg': -7
+                    },
+                    {
+                        'type': 'public-key',
+                        'alg': -257
+                    }
+                ]
+            }
+            console.log('Gen RSA Key Pair:', publicKey1)
+
+
+            return await new Promise(resolve => {navigator.credentials.create({
+                    'publicKey': publicKey1
+                })
+                .then((newCredentialInfo) => {
+
+                    console.log('SUCCESS', newCredentialInfo)
+                    console.log('ClientDataJSON: ', bufferToString(newCredentialInfo.response
+                        .clientDataJSON))
+                    let attestationObject = CBOR.decode(newCredentialInfo.response.attestationObject);
+                    console.log('AttestationObject: ', attestationObject)
+                    let authData = parseAuthData(attestationObject.authData);
+                    console.log('AuthData: ', authData);
+                    console.log('CredID: ', bufToHex(authData.credID));
+                    console.log('AAGUID: ', bufToHex(authData.aaguid));
+                    console.log('PublicKey', CBOR.decode(authData.COSEPublicKey.buffer));
+
+                    let returnData = showRSAKeyPair(authData.credID);
+                    document.getElementById('target_key_handle').innerText = bufToHex(returnData[1]);
+
+
+
+                })
+                .catch((error) => {
+                    alert(error)
+                    console.log('FAIL', error)
+                })
+            });
+}
+
 
 function base64EncodeURL(byteArray) {
     return btoa(Array.from(new Uint8Array(byteArray)).map(val => {
