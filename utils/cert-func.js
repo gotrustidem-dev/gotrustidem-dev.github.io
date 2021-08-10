e528a63cde6dd076de9cb55b7c5981f7ebaf1cc4
GuanHanChen <guanhan.chen@gotrustid.com>
2021-08-10 13:13:15 +0800

    update

--- utils/cert-func.js
+++ utils/cert-func.js
@@ -6,6 +6,7 @@
     
   window.addEventListener('load', () => {   
     document.getElementById('get_Cert_ByIndex_button').addEventListener("click", () => {
+      console.log("1");
       let certIndex = document.getElementById('target_cert_index').value;
       GetCertInfo(certIndex);       
     });                         
