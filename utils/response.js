b321531d4b867975137b6a0651defbf56f0ddd76
GuanHanChen <guanhan.chen@gotrustid.com>
2021-08-20 10:55:34 +0800

    update

---
+++ utils/response.js
@@ -0,0 +1,20 @@
+
+
+
+
+class GTIdemJs {
+
+
+    constructor() {
+        this.statusCode = "code";
+        this.fw= "fullName";
+        this.sw = "age";
+        this.pinRetry = "gender";
+        this.CredentialNum = "CredentialNum";
+        this.sn = "sn";
+        this.rn = "rn";
+        this.ecpoint = "ecpoint";
+    }
+
+
+}
