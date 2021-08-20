182a63c1925edf653356577b655d0c8f1063d464
GuanHanChen <guanhan.chen@gotrustid.com>
2021-08-20 12:04:06 +0800

    update

--- utils/response.js
+++ utils/response.js
@@ -1,6 +1,20 @@
+class Player {
+    constructor(fullName, age, gender, hairColor) {
+        this.fullName = fullName
+        this.age = age
+        this.gender = gender
+        this.hairColor = hairColor
+    }
+
+    toString() {
+        return 'Name: '+this.fullName+', Age:'+this.age
+    }
+}
+
+
+
+
 class GTIdemJs {
-
-
 
     constructor() {
         this.statusCode = undefined;
