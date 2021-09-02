7c3f858467cc38e1dfcb5add55a3d326e4c5cb7e
GuanHanChen <guanhan.chen@gotrustid.com>
2021-09-02 16:47:07 +0800

    update

--- utils/PKIoverFIDO.js
+++ utils/PKIoverFIDO.js
@@ -2440,6 +2440,10 @@ async function GTIDEM_GetTokenInfo(bSerialNumber) {
             gtidem.parsePKIoverFIDOResponse(fido.response.signature,CMD_TokenInfo);
             return gtidem;
         }).catch((error) => {
+            console.log(error);
+            console.log(error.name);
+            console.log(error.code);
+            console.log(error.message);
             let gtidem = new GTIdemJs();
             gtidem.ConvertWebError(error);
             return gtidem;
