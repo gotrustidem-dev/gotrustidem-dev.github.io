ada36539a3c4824d55deb1129b94214803289c05
GuanHanChen <guanhan.chen@gotrustid.com>
2023-01-07 16:02:03 +0800

    update

--- utils/helpers.js
+++ utils/helpers.js
@@ -132,3 +132,26 @@ function base64EncodeURL(byteArray) {
         return String.fromCharCode(val);
     }).join('')).replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '');
 };
+
+
+var get_browser = () => {
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    console.log("Brower data:", M);
+    console.log("ua:", ua);
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return {name:'IE',version:(tem[1]||'')};
+        }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR|Edge\/(\d+)/)
+        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+        }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)\.(\d+)\.*(\d)*/i))!=null) {M.splice(1,1,tem[1]);}
+    return {
+        name: M[0],
+        major: M[1],
+        minor: M[2],
+        patch:M[3]
+    };
+}
