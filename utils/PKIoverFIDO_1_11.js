2064dcf9be19b032b6aa3003695df6e6ac0c48cf
GuanHanChen <guanhan.chen@gotrustid.com>
2023-01-12 10:31:32 +0800

    update

--- utils/PKIoverFIDO_1_11.js
+++ utils/PKIoverFIDO_1_11.js
@@ -883,7 +883,7 @@ function GTIDEM_isValidTokenParams(bInitToken, commandType){
             return result;
         });
    
-    }, 500);
+    }, 400);
     //Generate 
     prepareUpdate = await computingSessionKey(bOldPIN, bNewPIN, bECPointFromToken);
 }
